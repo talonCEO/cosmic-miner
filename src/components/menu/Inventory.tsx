5fcbc2f109f710b2f397b43e216285d4c01ea5ed
talonCEO <karchibald98@outlook.com>
2025-03-16 21:46:29 +0000

    Update Inventory.tsx

--- src/components/menu/Inventory.tsx
+++ src/components/menu/Inventory.tsx
@@ -318,7 +318,7 @@ const Inventory: React.FC = () => {
           <span>Inventory</span>
         </DialogTitle>
         <div className="text-center text-slate-300 text-sm">
-          Space used: {inventoryUsed}/1
+          Space used: {inventoryUsed}/{inventoryCapacity}
         </div>
       </DialogHeader>
       
