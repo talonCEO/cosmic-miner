47543f0b1325a2ec12ea0900a1562ceb04721c29
talonCEO <karchibald98@outlook.com>
2025-03-20 09:35:38 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
@@ -59,8 +57,6 @@ const AnimatedRoutes = () => {
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
       <BrowserRouter>
         <AnimatedRoutes />
       </BrowserRouter>
