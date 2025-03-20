2fd25770e325435d0b7728bcf9a5c1185ef9bbb0
talonCEO <karchibald98@outlook.com>
2025-03-20 05:30:56 +0000

    Update GameContext.tsx

--- src/context/GameContext.tsx
+++ src/context/GameContext.tsx
@@ -299,7 +299,7 @@ const initialAbilities: Ability[] = [
 const UPGRADE_COST_GROWTH = 1.15;
 
 const initialState: GameState = {
-  coins: 1,000,000,000,000,000,
+  coins: 1000000000000000,
   coinsPerClick: 1,
   coinsPerSecond: 0,
   upgrades: upgradesList.map(upgrade => ({
