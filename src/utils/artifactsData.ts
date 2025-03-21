1e8c4581de7b5dbaee76ced870687e0e574313ff
talonCEO <karchibald98@outlook.com>
2025-03-21 14:06:03 +0000

    Update artifactsData.ts

--- src/utils/artifactsData.ts
+++ src/utils/artifactsData.ts
@@ -1,7 +1,7 @@
 import { Perk } from './types';
 
 // Static image imports for artifacts
-import ArtifactDefault from '@/assets/images/icons/a1.png';
+import ArtifactDefault from '@/assets/images/artifacts/art.png';
 import Artifact1 from '@/assets/images/artifacts/art10.png';
 import Artifact2 from '@/assets/images/artifacts/art9.png';
 import Artifact3 from '@/assets/images/artifacts/art8.png';
